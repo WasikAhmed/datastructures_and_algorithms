1e6fc5b47b58b9a8b62c02e09803f43908fc8224
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-07-14 23:58:33 +0600

    Added bubble sort implementation for sorting arrays

---
+++ algorithms/sorting_algorithms/bubble_sort.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr: list[int]):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [475, 149, 560, 379, 300, 339, 692, 291, 307, 283]
+    sorted_arr = bubble_sort(arr)
+    print(sorted_arr)
